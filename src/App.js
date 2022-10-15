3825a7763e141a025d6e832a6a4abb9475f8cda5
dashinghmaverick <rohitprasadswn@gmail.com>
2022-10-15 13:50:25 +0530

    login page

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import Create from './components/Create';
+import './App.css';
+
+function App() {
+  return (
+    <div className="main">
+      <h2 className="main-header">React Crud Operations</h2>
+    
+    <div>
+      <Create/>
+    </div>
+  </div>
+  );
+}
+
+export default App;
